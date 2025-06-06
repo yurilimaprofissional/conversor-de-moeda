6a2927ef7c0bc60f3310376255c9b20638217a12
Yuri Lima <yurilimaprofissional@gmail.com>
2025-06-05 22:17:09 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let form = document.getElementById("converterForm")
+const amount = document.getElementById("amount")
+const fromCurrency = document.getElementById("fromCurrency")
+const converteAmount = document.getElementById("converteAmount") 
+
+function convertMoney(){
+    console.log("")
+
+}
+
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+    console.log(event)
+})
