91e37e6a4f3c95ebf5d5a6b6bccfa5e2423c6c14
Yuri Lima <yurilimaprofissional@gmail.com>
2025-06-05 22:29:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let form = document.getElementById("converterForm")
+const form = document.getElementById("converterForm")
 const amount = document.getElementById("amount")
 const fromCurrency = document.getElementById("fromCurrency")
-const converteAmount = document.getElementById("converteAmount")
+const convertedAmount = document.getElementById("convertedAmount")
 const toCurrency = document.getElementById("toCurrency")
 const loading = document.querySelector(".loading") 
 const result = document.querySelector(".result")
+const error = document.querySelector(".error")
+
 
 function convertMoney(){
+    loading.style.display = "block"
     console.log("")
 
 }
